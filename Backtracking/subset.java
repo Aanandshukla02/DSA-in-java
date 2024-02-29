c9858af4d0ec52c5036742043c2be1ba0a7f83bc
Aanandshukla02 <aanandshukla05@gmail.com>
2024-02-29 22:03:09 +0530

    update

--- Backtracking/subset.java
+++ Backtracking/subset.java
@@ -1,5 +1,19 @@
 public class subset {
-    public static void main(String[] args) {
+    public static void findsubsets(String str,int i, String ans){
+        if (i==str.length()) {
+            if (ans.length()==0) {
+                System.out.println("Null");
+            }else{
+                System.out.println(ans);
+            }
+            return;
+        }
+        findsubsets(str, i+1, ans+str.charAt(i));
+        findsubsets(str, i+1, ans);
         
+    }
+    public static void main(String[] args) {
+        String str="ABC";
+        findsubsets(str, 0, "");
     }
 }
